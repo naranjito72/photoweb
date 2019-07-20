16a50dc986972bebaa80668511d46c4dbb0b1d9e
unknown <you@example.com>
2019-07-20 13:32:05 +0200

    7 commit

--- src/app/enlaces/enlaces.component.ts
+++ src/app/enlaces/enlaces.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FotografiasService } from '../services/fotografias.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-enlaces',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./enlaces.component.css']
 })
 export class EnlacesComponent implements OnInit {
+  public id: any;
+  public fotografia: any = {};
 
-  constructor() { }
+  constructor(public fotografiaService: FotografiasService, public route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.subscribe((params) => {
+    this.id = params.id;
+    this.fotografia = this.fotografiaService.getFotografia(this.id);
+    });
   }
 
 }
