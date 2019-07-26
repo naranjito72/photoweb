b6622346fb5fc835e77d2db1f097d82e2fb9aa2e
unknown <you@example.com>
2019-07-26 12:45:15 +0200

    notfound commit

--- src/app/commons/notfound/notfound.component.ts
+++ src/app/commons/notfound/notfound.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-notfound',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notfound.component.css']
 })
 export class NotfoundComponent implements OnInit {
-
+  @Output() public sidenavToggle = new EventEmitter();
+  public isLogged = false;
   constructor() { }
 
   ngOnInit() {
